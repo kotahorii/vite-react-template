c0ab8aebb89f5840aeecd8c2f99deb05de406bbe
kotahorii <khorii.micron@gmail.com>
2022-01-16 13:24:02 +0900

    [Update] redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import { Provider } from 'react-redux'
 import App from './App'
+import { store } from './app/store'
 import './index.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/slices/appSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '../app/store'
+import { Task } from '../types/types'
+
+export type AppState = {
+  editedTask: Task
+}
+const initialState: AppState = {
+  editedTask: {
+    id: '',
+    title: '',
+    description: '',
+  },
+}
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    setEditedTask: (state, action: PayloadAction<Task>) => {
+      state.editedTask = action.payload
+    },
+    resetEditedTask: (state) => {
+      state.editedTask = initialState.editedTask
+    },
+  },
+})
+export const { setEditedTask, resetEditedTask } = appSlice.actions
+
+export const selectTask = (state: RootState) => state.app.editedTask
+export default appSlice.reducer
